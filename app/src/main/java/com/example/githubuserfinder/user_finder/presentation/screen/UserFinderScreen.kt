c8e3cc9273cb9825af6569789b8332492305f579
Alireza Abiri <abiriali78@gmail.com>
2022-05-03 20:03:40 +0430

    chore: Created UserFinder module and the screen file

---
+++ app/src/main/java/com/example/githubuserfinder/user_finder/presentation/screen/UserFinderScreen.kt
@@ -0,0 +1,10 @@
+package com.example.githubuserfinder.user_finder.presentation.screen
+
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+
+@Composable
+fun UserFinderScreen() {
+    Scaffold {
+    }
+}
