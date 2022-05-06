c84ce3c532795439f98d2292c893a3b878b324e7
Alireza Abiri <abiriali78@gmail.com>
2022-05-06 13:53:40 +0430

    refactor: Removed unnecessary clone of uiState flow

--- app/src/main/java/com/example/githubuserfinder/user_detail/presentation/viewmodel/UserDetailViewModel.kt
+++ app/src/main/java/com/example/githubuserfinder/user_detail/presentation/viewmodel/UserDetailViewModel.kt
@@ -7,9 +7,6 @@ import com.example.githubuserfinder.user_detail.data.model.GithubUserDetailModel
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.ensureActive
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
 data class UserDetailUiState(
@@ -23,17 +20,11 @@ class UserDetailViewModel(
 
     var dataFetchingJob: Job? = null
 
-    private val _uiState = MutableStateFlow(UserDetailUiState())
-    val uiState: StateFlow<UserDetailUiState> = _uiState
-        .stateIn(
-            viewModelScope,
-            SharingStarted.WhileSubscribed(5000),
-            _uiState.value,
-        )
+    val uiState = MutableStateFlow(UserDetailUiState())
 
     fun fetchUserData(username: String): Job = viewModelScope.launch {
-        _uiState.emit(
-            _uiState.value.copy(
+        uiState.emit(
+            uiState.value.copy(
                 isLoading = true
             )
         )
@@ -41,14 +32,14 @@ class UserDetailViewModel(
         val result = usersRemoteDataSource.getUser(username)
         ensureActive()
         if (result != null) {
-            _uiState.emit(
-                _uiState.value.copy(
+            uiState.emit(
+                uiState.value.copy(
                     userData = result,
                 )
             )
         }
-        _uiState.emit(
-            _uiState.value.copy(
+        uiState.emit(
+            uiState.value.copy(
                 isLoading = false,
             )
         )
