650fc2e80fa9bbdb75d92a2db634f80a053a177c
Alireza Abiri <abiriali78@gmail.com>
2022-05-10 00:49:34 +0430

    security: Added google play services automatic security provider installer

--- app/src/main/java/com/example/githubuserfinder/app/MainActivity.kt
+++ app/src/main/java/com/example/githubuserfinder/app/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.example.githubuserfinder.app
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.core.view.WindowCompat
 import com.example.githubuserfinder.app.theme.lightThemeColors
+import com.google.android.gms.common.GoogleApiAvailability
+import com.google.android.gms.security.ProviderInstaller
 
 const val TAG = "MainActivity"
 
+private const val ERROR_DIALOG_REQUEST_CODE = 1
+
 // Dependencies are created manually in the main activity and injected to the lower layers
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(), ProviderInstaller.ProviderInstallListener {
+
+    private var retryProviderInstall: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -23,5 +31,66 @@ class MainActivity : ComponentActivity() {
                 GithubUserFinderApp()
             }
         }
+    }
+
+    /**
+     * This method is only called if the provider is successfully updated
+     * (or is already up-to-date).
+     */
+    override fun onProviderInstalled() {
+        // Provider is up-to-date, app can make secure network calls.
+    }
+
+    /**
+     * This method is called if updating fails; the error code indicates
+     * whether the error is recoverable.
+     */
+    override fun onProviderInstallFailed(errorCode: Int, recoveryIntent: Intent?) {
+        GoogleApiAvailability.getInstance().apply {
+            if (isUserResolvableError(errorCode)) {
+                // Recoverable error. Show a dialog prompting the user to
+                // install/update/enable Google Play services.
+                showErrorDialogFragment(this@MainActivity, errorCode, ERROR_DIALOG_REQUEST_CODE) {
+                    // The user chose not to take the recovery action
+                    onProviderInstallerNotAvailable()
+                }
+            } else {
+                onProviderInstallerNotAvailable()
+            }
+        }
+    }
+
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int,
+        data: Intent?
+    ) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == ERROR_DIALOG_REQUEST_CODE) {
+            // Adding a fragment via GoogleApiAvailability.showErrorDialogFragment
+            // before the instance state is restored throws an error. So instead,
+            // set a flag here, which will cause the fragment to delay until
+            // onPostResume.
+            retryProviderInstall = true
+        }
+    }
+
+    /**
+     * On resume, check to see if we flagged that we need to reinstall the
+     * provider.
+     */
+    override fun onPostResume() {
+        super.onPostResume()
+        if (retryProviderInstall) {
+            // We can now safely retry installation.
+            ProviderInstaller.installIfNeededAsync(this, this)
+        }
+        retryProviderInstall = false
+    }
+
+    private fun onProviderInstallerNotAvailable() {
+        // This is reached if the provider cannot be updated for some reason.
+        // App should consider all HTTP communication to be vulnerable, and take
+        // appropriate action.
     }
 }
